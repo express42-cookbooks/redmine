66d19bd91b265b3d290dbebdf29558d3d2613ed4
Chris Rosser <chris@bluefuton.com>
2012-12-12 17:16:42 +0000

    Disable default Apache vhost

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,6 +23,11 @@ include_recipe "apache2"
 include_recipe "apache2::mod_rewrite"
 include_recipe "passenger_apache2::mod_rails"
 
+execute "disable-default-site" do
+  command "sudo a2dissite default"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
+
 execute "apt-get install imagemagick libmagickwand-dev"
 
 bash "install_redmine" do
